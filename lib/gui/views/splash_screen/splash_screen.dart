2fe654c991790d0cf5c00e73c41cc61479820b26
Stiven Gomez <stivengomez@soyyo.co>
2023-09-15 13:05:29 -0500

    Fix splashScreen.

--- lib/gui/views/splash_screen/splash_screen.dart
+++ lib/gui/views/splash_screen/splash_screen.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:shopbeer/gui/app_style.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({ Key? key }) : super( key: key );
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    Future.delayed( const Duration(seconds: 3), () {
+      Navigator.pushNamedAndRemoveUntil(context, 'login', (route) => false);
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: primaryColor,
       body: Center(
-        child: Image(
-          width: 215.0,
-          image: AssetImage('assets/iOS_icon.png')
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image(
+              width: 215.0,
+              image: AssetImage('assets/iOS_icon.png')
+            ),
+            const SizedBox(height: 30.0),
+            CircularProgressIndicator(
+              color: whiteColor,
+            )
+          ],
         ),
       ),
     );
