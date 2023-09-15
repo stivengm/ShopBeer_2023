ca72484a9afb208725ecae7eaaf40e3b2d7a104d
Stiven Gomez <stivengomez@soyyo.co>
2023-09-15 12:16:13 -0500

    Add appBarWidget from home.

--- lib/gui/views/dashboard_view/dashboard_view.dart
+++ lib/gui/views/dashboard_view/dashboard_view.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:shopbeer/gui/widgets/appbar_widget.dart';
 
 class DashboardView extends StatelessWidget {
   const DashboardView({ Key? key }) : super( key: key );
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBarGeneralWidget(titleAppbar: 'ShopBeer', fontFamily: 'Curved', fontSize: 30.0,),
+      body: Placeholder(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add
+        ),
+        onPressed: () {},
+      ),
+    );
   }
 }
---
+++ lib/gui/widgets/appbar_widget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:shopbeer/gui/app_style.dart';
+
+class AppBarGeneralWidget extends StatelessWidget with PreferredSizeWidget {
+  final String? titleAppbar;
+  final bool? actions;
+  final String? fontFamily;
+  final double? fontSize;
+  const AppBarGeneralWidget({ Key? key, this.titleAppbar, this.actions = false, this.fontFamily, this.fontSize }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: primaryColor,
+      title: Text(titleAppbar ?? '', style: Theme.of(context).textTheme.headline6!.copyWith(fontFamily: fontFamily ?? 'Gordita', fontSize: fontSize, color: whiteColor)),
+      // leading: IconButton(
+      //   icon: SvgPicture.asset('assets/icons/menu.svg', color: whiteColor,),
+      //   onPressed: () => ZoomDrawer.of(context)!.toggle()
+      // ),
+      actions: [
+        IconButton(
+          icon: const Icon(
+            Icons.notifications_none_rounded
+          ),
+          onPressed: () => Navigator.pushNamed(context, 'notifications'),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+}
