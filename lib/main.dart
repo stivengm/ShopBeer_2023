6515720044e74614f73070d0d0e33a1b2b2eb102
Stiven Gomez <stivengomez@soyyo.co>
2023-09-14 22:58:45 -0500

    Fix name app.

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
     ]);
 
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ShopBeer',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
